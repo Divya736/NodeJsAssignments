1a898b97fc25f0292925622a68cc06177d2a36c9
Divya736 <124857149+Divya736@users.noreply.github.com>
2023-02-22 11:42:26 +0530

    Add files via upload

---
+++ src/components/functional component/ParentComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Message from './Message'
+
+function ParentComponent() {
+  return (
+  <>
+  <Message name ="divya"/>
+  <Message name ="neenu"/>
+  <Message name ="ammu"/>
+  </>
+  )
+}
+
+export default ParentComponent
