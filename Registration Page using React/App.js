16c326cb45785ed0862b4f1132196ed124873ff8
Divya736 <124857149+Divya736@users.noreply.github.com>
2023-02-22 11:11:08 +0530

    Add files via upload

---
+++ Registration Page using React/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import StudentRegistration from './components/functional-comp/StudentRegistration';
+function App() {
+  return (
+    <div className="App">
+     <StudentRegistration/>
+   
+    </div>
+  );
+}
+
+export default App;
