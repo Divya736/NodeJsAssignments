fa8bd335be211ed804b891dd1d02a7ee0b8f7233
Divya736 <124857149+Divya736@users.noreply.github.com>
2023-03-10 19:03:15 +0530

    Add files via upload

---
+++ FinalAssesment/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Login from './Component/Login';
+import Registration from './Component/Registration';
+import { createBrowserRouter,RouterProvider} from 'react-router-dom';
+import Dashboard from './Component/Dashboard';
+import Password from './Component/Password';
+import Post from './Component/List/Post';
+import ToDo from './Component/List/ToDos';
+const router= createBrowserRouter([
+  {path:'/',element:<Login/> },
+  {path:'/register',element:<Registration/> },
+  {path:'/dashboard',element:<Dashboard/> },
+  {path:'/password',element:<Password/> },
+  {path:'/post',element:<Post/>},
+  {path:'/todo',element:<ToDo/>},
+])
+function App() {
+  return (
+    
+    <RouterProvider router={router}/>
+  );
+}
+
+export default App;
